88de5e132c72614359e477e149d3d22ed139b09c
Njorogeted <njorogeted883@gmail.com>
2023-09-11 11:09:30 -0700

    c is fun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<unistd.h>
+/**
+ * main - program that taces characters and outputs
+ * them in the standard error message
+ * ujumbe - message being delivered
+ * Return : return 1 when success
+ */
+int main(void)
+{
+	char *ujumbe = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2,ujumbe,61);
+	return (1);
+}
