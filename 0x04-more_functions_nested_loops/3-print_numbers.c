88c5044fe298b03c9515cf1625a70ebd02fbc086
Njorogeted <njorogeted883@gmail.com>
2023-09-14 07:19:12 -0700

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - progrem that prints number
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+		_putchar (x);
+	x++;
+}
+
