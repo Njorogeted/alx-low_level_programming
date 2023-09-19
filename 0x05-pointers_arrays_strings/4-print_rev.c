93944e128576fbd8a388edf9f1a05849ef17f54b
Njorogeted <njorogeted883@gmail.com>
2023-09-19 09:40:17 -0700

    4 reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints output in revers
+ * @s: prototipe for our function
+ * Return: return void
+ */
+void print_rev(char *s)
+{
+	int x;
+	int l = 1;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (x = l; x >= 0; x--)
+	{
+		_putchar (s[x]);
+	}
+	_putchar ('\n');
+}
