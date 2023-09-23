12cd20b8b90d799af605ba2673dd2214ec054428
Njorogeted <njorogeted883@gmail.com>
2023-09-23 05:47:18 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function to add two nubers
+ * x: first parameter
+ * y:second parameter
+ * Return: return results
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		{
 			if (y == 0)
 			{
-				printf ("%d", x * y);
+				printf("%d", x * y);
 			}
 			else
 			{
