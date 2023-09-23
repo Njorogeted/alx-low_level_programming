822a96f47ac387dda60d63cd868c73bc131b2f15
Njorogeted <njorogeted883@gmail.com>
2023-09-23 06:39:52 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - function to add two nubers
- * x: first parameter
- * y:second parameter
+ * @x: first parameter
+ * @y:second parameter
  * Return: return results
  */
 int add(int x, int y)
