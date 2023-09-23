f2b1d63e17cf47365ac26c34a1de9b756fbb122e
Njorogeted <njorogeted883@gmail.com>
2023-09-23 11:02:21 -0700

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that adds the multiples of 3 and 5
+ * Return: void
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			y += x;
+		}		
+	}
+	printf("%d\n", y);
+	return (0);
+}
